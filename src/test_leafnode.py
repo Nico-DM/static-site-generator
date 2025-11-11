d7f037bdfb5cab9b6bd769532a54e0b2158a97a6
Nico-DM <nicomgaletto@gmail.com>
2025-11-10 21:38:03 -0300

    add LeafNode class implementation and corresponding unit tests

---
+++ src/test_leafnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_span_with_props(self):
+        node = LeafNode("span", "Important", {"class": "highlight"})
+        self.assertEqual(node.to_html(), '<span class="highlight">Important</span>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
